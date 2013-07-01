91756662fcb26eee8a9615b1e496f903388e1c68
hortinstein <hortinstein@gmail.com>
2013-07-01 23:39:41 +0900

    cleaned up build scripts and installs service under swarmlicant instead of root

--- swarmigin.sh
+++ swarmigin.sh
@@ -24,6 +24,26 @@ su -c mkdir swarmlicant
 mkdir /home/swarmlicant/logs 
 chown swarmlicant:adm /home/swarmlicant/logs 
 
+
+tar -zxf node-v$NODE_VERSION.tar.gz
+echo 'installing node.js $NODE_VERSION'
+cd ~/node-install/node-v$NODE_VERSION
+./configure && make && checkinstall --install=yes --pkgname=nodejs --pkgversion "$NODE_VERSION" --default
+npm install -g nave
+nave install latest
+echo 'node.js install completed'
+
+
+# Install erlang
+mkdir ~/erlang-install
+cd ~/erlang-install
+echo 'installing erlang'
+wget http://erlang.org/download/otp_src_$ERLANG_VERSION.tar.gz
+tar -zxf otp_src_$ERLANG_VERSION.tar.gz
+cd ~/erlang-install/otp_src_$ERLANG_VERSION
+./configure && make && sudo make install
+echo 'erlang install complete'
+
 echo 'running new commands as user'
 chmod +x swarmlicant_build.sh
 chown -R swarmlicant:swarmlicant ./
