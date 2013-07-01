813718d66f15be7dd0577d340982e2669258119c
Alex Hortin <hortinstein@gmail.com>
2013-07-02 00:05:53 +0900

    Update swarmlicant_build.sh

--- swarmlicant_build.sh
+++ swarmlicant_build.sh
@@ -2,30 +2,6 @@
 
 RIAK_VERSION="1.3.2"
 
-# Install node.js
-mkdir ~/node-install
-cd ~/node-install
-wget "http://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.gz"
-
-tar -zxf node-v$NODE_VERSION.tar.gz
-echo 'installing node.js $NODE_VERSION'
-cd ~/node-install/node-v$NODE_VERSION
-./configure && make && checkinstall --install=yes --pkgname=nodejs --pkgversion "$NODE_VERSION" --default
-npm install -g nave
-nave install latest
-echo 'node.js install completed'
-
-
-# Install erlang
-mkdir ~/erlang-install
-cd ~/erlang-install
-echo 'installing erlang'
-wget http://erlang.org/download/otp_src_$ERLANG_VERSION.tar.gz
-tar -zxf otp_src_$ERLANG_VERSION.tar.gz
-cd ~/erlang-install/otp_src_$ERLANG_VERSION
-./configure && make && sudo make install
-echo 'erlang install complete'
-
 # Install riak
 cd ~/
 mkdir ~/riak-install
